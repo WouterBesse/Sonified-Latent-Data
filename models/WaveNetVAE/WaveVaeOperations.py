fb1fe283f673a7308ed3a71229c03909bd33ec8b
Wouter <wouterbesse@gmail.com>
2023-06-02 10:55:59 +0100

    Refactored WaveVaeOperations

--- models/WaveNetVAE/WaveVaeOperations.py
+++ models/WaveNetVAE/WaveVaeOperations.py
@@ -6,19 +6,8 @@ from torch import Tensor
 from torch.nn import functional as F
 
 """
-Util functions
-"""    
-def normalisedConvTranspose2d(in_channels, out_channels, kernel_size,
-                    weight_normalization=True, **kwargs):
-    freq_axis_kernel_size = kernel_size[0]
-    m = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, **kwargs)
-    m.weight.data.fill_(1.0 / freq_axis_kernel_size)
-    m.bias.data.zero_()
-    if weight_normalization:
-        return nn.utils.weight_norm(m)
-    else:
-        return m
-
+Weight initialisation functions
+"""
 def xavieru_init(mod):
     if hasattr(mod, 'weight') and mod.weight is not None:
         nn.init.xavier_uniform_(mod.weight, gain=nn.init.calculate_gain('leaky_relu'))
@@ -131,7 +120,6 @@ class ResidualConv1dGLU(nn.Module):
     def __init__(self, residual_channels, gate_channels, kernel_size, skip_out_channels = None, cin_channels = -1, dropout= 1 - 0.95, dilation = 1, bias = False, init_type='kaiming_n'	):
         super(ResidualConv1dGLU, self).__init__()
 
-        # self.dropout = nn.Dropout(p = dropout)
 #       dilations = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
 
         self.dil_conv = CausalConvolution1D(residual_channels, 
@@ -173,20 +161,16 @@ class ResidualConv1dGLU(nn.Module):
             x (Tensor): B x C x T
             c (Tensor): B x C x T, Local conditioning features
         Returns:
-            Tensor: output
+            x (Tensor): B x residual_channels x T
+            s (Tensor): B x skip_channels x T
         """
         residual = x
-        # x = self.dropout(x)
         condition = self.conv1cond(c)
 
         x = self.dil_conv(x) # Dilated convolution
 
-        # x = x[:, :, :residual.size(-1)] # Remove future time steps
-
         a, b = x.split(x.size(self.splitdim) // 2, dim=self.splitdim) # Get filter and gate
-
-        # local conditioning
-        ca, cb = condition.split(condition.size(self.splitdim) // 2, dim=self.splitdim) # Get filter and gate from condition
+        ca, cb = condition.split(condition.size(self.splitdim) // 2, dim=self.splitdim) # Get filter and gate from local condition
         filt, gate = a + ca, b + cb # Combine filters and gates
 
         x = torch.tanh(filt) * torch.sigmoid(gate)
@@ -196,67 +180,15 @@ class ResidualConv1dGLU(nn.Module):
 
         # For residual connection
         x = self.conv1_out(x)
-        # x = (x + residual) * math.sqrt(0.5)
         x += residual
         
         return x, s
-
-class Stretch2d(nn.Module):
-    def __init__(self, x_scale, y_scale, mode="nearest"):
-        super(Stretch2d, self).__init__()
-        self.x_scale = x_scale
-        self.y_scale = y_scale
-        self.mode = mode
-
-    def forward(self, x):
-        return F.interpolate(
-            x, scale_factor=(self.y_scale, self.x_scale), mode=self.mode)
-
-    
-def _get_activation(upsample_activation):
-    nonlinear = getattr(nn, upsample_activation)
-    return nonlinear
-
-class UpsampleNetwork(nn.Module):
-    def __init__(self, upsample_scales, upsample_activation="none",
-                 upsample_activation_params={}, mode="nearest",
-                 freq_axis_kernel_size=1, cin_pad=0, cin_channels=80):
-        super(UpsampleNetwork, self).__init__()
-        self.up_layers = nn.ModuleList()
-        total_scale = np.prod(upsample_scales)
-        self.indent = cin_pad * total_scale
-        for scale in upsample_scales:
-            freq_axis_padding = (freq_axis_kernel_size - 1) // 2
-            k_size = (freq_axis_kernel_size, scale * 2 + 1)
-            padding = (freq_axis_padding, scale)
-            stretch = Stretch2d(scale, 1, mode)
-            conv = nn.Conv2d(1, 1, kernel_size=k_size, padding=padding, bias=False)
-            conv.weight.data.fill_(1. / np.prod(k_size))
-            conv = nn.utils.weight_norm(conv)
-            self.up_layers.append(stretch)
-            self.up_layers.append(conv)
-            if upsample_activation != "none":
-                nonlinear = _get_activation(upsample_activation)
-                self.up_layers.append(nonlinear(**upsample_activation_params))
-
-    def forward(self, c):
-        """
-        Args:
-            c : B x C x T
-        """
-
-        # B x 1 x C x T
-        c = c.unsqueeze(1)
-        for f in self.up_layers:
-            c = f(c)
-        # B x C x T
-        c = c.squeeze(1)
-
-        if self.indent > 0:
-            c = c[:, :, self.indent:-self.indent]
-        return c
     
 class Upsampling(nn.Module):
+    """
+    Module for upsampling conditional features
+    Sourced from: https://github.com/hrbigelow/ae-wavenet/blob/master/wavenet.py#L142
+    """
     def __init__(self, n_chan, filter_sz, stride, bias=True, name=None, init_type='kaiming_n'):
         super(Upsampling, self).__init__()
         # See upsampling_notes.txt: padding = filter_sz - stride
